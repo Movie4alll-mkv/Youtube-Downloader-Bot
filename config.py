3c6c046dbf5948ee2b4b217a6670ac93c0edc428
Movie4alll-mkv <77849768+Movie4alll-mkv@users.noreply.github.com>
2023-01-07 12:39:26 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-APP_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN","5442493323:AAHPw8TNe0hh2zCAQKm_2O2o6KdmQ3Okgf8")
+APP_ID = int(os.environ.get("API_ID","6534707"))
+API_HASH = os.environ.get("API_HASH","4bcc61d959a9f403b2f20149cbbe627a")
 
 youtube_next_fetch = 0  # time in minute
 
